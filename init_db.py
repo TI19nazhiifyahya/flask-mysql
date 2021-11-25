2f4152d4ac9e176b957ddd67b8bdbec67c3607ef
Ardian0912 <muhammadnurwidya.2019@student.uny.ac.id>
2021-11-25 20:06:46 +0700

    (M. Nurwidya A) mengubah sqlite3 menjadi mysql di init_db.py

--- init_db.py
+++ init_db.py
@@ -1,19 +1,29 @@
-import sqlite3
+# import sqlite3
+import mysql.connector
 
-connection = sqlite3.connect('database.db')
+# connection = sqlite3.connect('database.db')
+connection = mysql.connector.connect(
+    hostname = "localhost",
+    user = "root",
+    password = "",
+    database = "database"
+)
 
-with open('schema.sql') as f:
-    connection.executescript(f.read())
+# with open('schema.sql') as f:
+#     connection.executescript(f.read())
 
 cur = connection.cursor()
+cur.execute("DROP TABLE IF EXISTS posts")
+cur.execute("CREATE TABLE posts ( id INT PRIMARY KEY AUTO_INCREMENT, created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, title TEXT NOT NULL, content TEXT NOT NULL )")
+cur.execute(
+    "INSERT INTO posts (title, content) VALUES (%s, %s)",
+            ('First Post', 'Content for the first post')
+)
 
-cur.execute("INSERT INTO posts (title, content) VALUES (?, ?)",
-            ('First Post', 'Contect for the first post')
-            )
-
-cur.execute("INSERT INTO posts (title, content) VALUES (?, ?)",
-            ('Second Post', 'Content for the second post')
-            )
+cur.execute(
+    "INSERT INTO posts (title, content) VALUES (%s, %s)",
+    ('Second Post', 'Content for the second post')
+)
 
 connection.commit()
 connection.close()
